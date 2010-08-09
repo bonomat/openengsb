1337feefd81f50a7a7de81561a78d265f957e209
Philipp Hoenisch <philipp.hoenisch@gmail.com>
2010-08-09 12:52:00 +0200

    Properties file renamed
    Signed-off-by: Philipp Hoenisch <philipp.hoenisch@gmail.com>

---
+++ ui/web/src/test/java/org/openengsb/ui/web/TestClientTest.java
@@ -0,0 +1,11 @@
+package org.openengsb.ui.web;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bonomat
+ * Date: 09.08.2010
+ * Time: 09:51:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestClientTest {
+}
